b7ea62b67657d4d4df866facb09cb9093aafd50e
ebuka1017 <isaacokwuzi@gmail.com>
2023-10-05 18:01:46 -0700

    task 2

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,3 +1,6 @@
 #!/usr/bin/env ruby
-#task 2
-puts ARGV[0].scan(/hb{0,1}tn/)
+# This script accepts one argument and passes it to a regular expression,
+# matching method
+# The regular expression must match the given cases
+
+puts ARGV[0].scan(/hb?tn/).join
